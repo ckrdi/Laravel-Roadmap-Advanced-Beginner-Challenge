ada72e73ef762311b800bd884e2fec52bf5b210f
ckrdi <agung.cokorda@gmail.com>
2021-09-02 19:42:56 +0800

    Add roles and permissions to users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,12 +14,25 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create([
+        $super = User::create([
             'name' => 'Obi-wan Kenobi',
             'email' => 'generalkenobi@gmail.com',
             'email_verified_at' => now(),
             'password' => bcrypt('secret')
         ]);
-        User::factory(9)->create();
+
+        $super->assignRole('Super Admin');
+        
+        $admins = User::factory(3)->create();
+
+        foreach ($admins as $admin) {
+            $admin->assignRole('Admin');
+        }
+
+        $users = User::factory(6)->create();
+
+        foreach ($users as $user) {
+            $user->assignRole('User');
+        }
     }
 }
